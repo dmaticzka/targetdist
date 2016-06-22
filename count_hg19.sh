33a2afadb66faf85b43dcb924101be293541530f
Daniel Maticzka <maticzkd@informatik.uni-freiburg.de>
2016-06-22 14:59:54 +0200

    make callable from anywhere

--- count_hg19.sh
+++ count_hg19.sh
@@ -10,19 +10,19 @@ cat $1 | cut -f 1-6 | \
 bedtools annotate -counts -s \
 -i - \
 -files \
-annotation/hg19/type_rRNA.bed \
-annotation/hg19/tRNAgenes.bed \
-annotation/hg19/type_snoRNA.bed \
-annotation/hg19/type_snRNA.bed \
-annotation/hg19/type_lincRNA.bed \
-annotation/hg19/type_misc_ncRNA.bed \
-annotation/hg19/type_pseudogene.bed \
-annotation/hg19/type_protein_coding_antisense.bed \
-annotation/hg19/3utr.bed \
-annotation/hg19/5utr.bed \
-annotation/hg19/codex.bed \
-annotation/hg19/introns.bed \
-annotation_antisense.bed \
+$BASEDIR/annotation/hg19/type_rRNA.bed \
+$BASEDIR/annotation/hg19/tRNAgenes.bed \
+$BASEDIR/annotation/hg19/type_snoRNA.bed \
+$BASEDIR/annotation/hg19/type_snRNA.bed \
+$BASEDIR/annotation/hg19/type_lincRNA.bed \
+$BASEDIR/annotation/hg19/type_misc_ncRNA.bed \
+$BASEDIR/annotation/hg19/type_pseudogene.bed \
+$BASEDIR/annotation/hg19/type_protein_coding_antisense.bed \
+$BASEDIR/annotation/hg19/3utr.bed \
+$BASEDIR/annotation/hg19/5utr.bed \
+$BASEDIR/annotation/hg19/codex.bed \
+$BASEDIR/annotation/hg19/introns.bed \
+$BASEDIR/annotation/hg19/annotation_antisense_hg19.bed \
 -names rRNA tRNA snoRNA snRNA lincRNA misc_ncRNA pseudogene protcod_as 3UTR 5UTR exon intron antisense | \
-bin/countAnnot.pl | \
-bin/colAdd.sh $BED > $CSV
+$BASEDIR/bin/countAnnot.pl | \
+$BASEDIR/bin/colAdd.sh $BED > $CSV
